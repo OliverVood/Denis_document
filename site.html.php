e11dbbd6f44b0d53bb7cc602c93d67735cdc1abc
Денис <gomandenis@mail.ru>
2023-06-06 10:34:19 +0700

    Restructure site units. Division into html & xhr

---
+++ site.html.php
@@ -0,0 +1,7 @@
+<?php
+
+	require __DIR__ . '/consts.inc';
+
+	if (!POINT_ENTRY_SITE) die('Доступ запрещён');
+
+	require DIR_ROOT . 'proj/site.html.inc';
--- site.xhr.php
+++ site.xhr.php
@@ -4,4 +4,4 @@
 
 	if (!POINT_ENTRY_SITE) die('Доступ запрещён');
 
-	require DIR_ROOT . 'proj/site.inc';
+	require DIR_ROOT . 'proj/site.xhr.inc';
